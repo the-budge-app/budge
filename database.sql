efeac8812edcbd2917818ce58b5a6d2fe98920bf
Weizhen Wang <wangwzhen@gmail.com>
2019-07-26 16:13:53 -0500

    update database name at sql file and pool.js

--- database.sql
+++ database.sql
@@ -3,7 +3,9 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
---user table
+
+--database name at localhost: budge_app
+
 --user table
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
