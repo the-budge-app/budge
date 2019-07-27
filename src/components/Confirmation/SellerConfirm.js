2420d59ec8fdb5af692aa2763b54696badd41dd6
KaetiG <kaetigust@gmail.com>
2019-07-27 15:46:17 -0500

    SelectedOffer Confirmations and Payment pages created

---
+++ src/components/Confirmation/SellerConfirm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class SellerConfirm extends Component {
+    render() {
+        return (
+            <>
+            <h1>Seller Confirm Page</h1>
+            </>
+        )
+    }
+}
+
+export default connect(SellerConfirm);
---
+++ src/components/SelectedOffer/SelectedOffer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class SelectedOffer extends Component {
+    render() {
+        return (
+            <>
+            <h1>Selected Offer Page</h1>
+            </>
+        )
+    }
+}
+
+export default connect(SelectedOffer);
