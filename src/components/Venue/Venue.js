b8e4f1d232f87788838e6cf7d5dc762512570261
Weizhen Wang <wangwzhen@gmail.com>
2019-07-29 16:23:25 -0500

    add button highlight for user's own waitlist

--- src/components/Venue/Venue.js
+++ src/components/Venue/Venue.js
@@ -24,7 +24,7 @@ class Venue extends Component {
         })
 
         this.props.dispatch({
-            type:'FETCH_SELECTED_VENUE',
+            type: 'FETCH_SELECTED_VENUE',
             payload: this.props.match.params.id,
         })
     }
@@ -41,38 +41,51 @@ class Venue extends Component {
     render() {
         const { active } = this.state
         return (
-                <div style={styles.mainDiv}>
-                    <h3>{this.props.selectedVenue.restaurant_name}</h3>
-                    <h4>{this.props.selectedVenue.phone_number && this.props.selectedVenue.phone_number.substr(0, 3)} - {this.props.selectedVenue.phone_number && this.props.selectedVenue.phone_number.substr(3, 3)} - {this.props.selectedVenue.phone_number && this.props.selectedVenue.phone_number.substr(6, 4)} </h4>
-                    <h4>{this.props.selectedVenue.address}</h4>
-                    <h4>{this.props.selectedVenue.city} {this.props.selectedVenue.state}, {this.props.selectedVenue.zip}</h4>
+            <div style={styles.mainDiv}>
+                <h3>{this.props.selectedVenue.restaurant_name}</h3>
+                <h4>{this.props.selectedVenue.phone_number && this.props.selectedVenue.phone_number.substr(0, 3)} - {this.props.selectedVenue.phone_number && this.props.selectedVenue.phone_number.substr(3, 3)} - {this.props.selectedVenue.phone_number && this.props.selectedVenue.phone_number.substr(6, 4)} </h4>
+                <h4>{this.props.selectedVenue.address}</h4>
+                <h4>{this.props.selectedVenue.city} {this.props.selectedVenue.state}, {this.props.selectedVenue.zip}</h4>
 
-                    <h3>Waitlist</h3>
+                <h3>Waitlist</h3>
 
-                    <label>Budgable</label>
-                    <Checkbox toggle></Checkbox>
-                    <label>All Parties</label>
-                    <br />
-                    <br />
-                    <br />
-                    {this.props.venueInfo.map(venue =>
-                        <>
-                            <Button key={venue.waitlist_id} fluid onClick={()=>this.handleClick(venue.waitlist_id)}>
-                                <Icon name="user" />{venue.party_size}
-                                <Icon name="clock" />{venue.quote_time}
-                                <Icon name="dont" />
-                                    $ {venue.rejected_price[0]}
-                                
-                            </Button>
-                            <br />
-                        </>
-                    )}
-                    {this.state.active ?
-                        <Button fluid toggle active={active} onClick={this.toggleButton}>Join Waitlist</Button>
-                        :
-                        <Button fluid toggle active={active} onClick={this.toggleButton}>Leave Waitlist</Button>
-                    }
-                </div>
+                <label>Budgable</label>
+                <Checkbox toggle></Checkbox>
+                <label>All Parties</label>
+                <br />
+                <br />
+                <br />
+                {this.props.venueInfo.map(venue => 
+                venue.user_id === this.props.user.id ?
+                    <>
+                        <Button key={venue.waitlist_id} fluid primary onClick={() => this.handleClick(venue.waitlist_id)}>
+                            <Icon name="user" />{venue.party_size}
+                            <Icon name="clock" />{venue.quote_time}
+                            <Icon name="dont" />
+                            $ {venue.rejected_price[0]}
+
+                        </Button>
+                        <br />
+                    </>
+                    :
+                    <>
+                        <Button key={venue.waitlist_id} fluid onClick={() => this.handleClick(venue.waitlist_id)}>
+                            <Icon name="user" />{venue.party_size}
+                            <Icon name="clock" />{venue.quote_time}
+                            <Icon name="dont" />
+                            $ {venue.rejected_price[0]}
+
+                        </Button>
+                        <br />
+                    </>
+
+                )}
+                {this.state.active ?
+                    <Button fluid toggle active={active} onClick={this.toggleButton}>Join Waitlist</Button>
+                    :
+                    <Button fluid toggle active={active} onClick={this.toggleButton}>Leave Waitlist</Button>
+                }
+            </div>
         )
     }
 }
@@ -80,5 +93,6 @@ class Venue extends Component {
 const mapStateToProps = reduxState => ({
     venueInfo: reduxState.venueInfo,
     selectedVenue: reduxState.selectedVenue,
+    user: reduxState.user,
 });
 export default connect(mapStateToProps)(Venue);
