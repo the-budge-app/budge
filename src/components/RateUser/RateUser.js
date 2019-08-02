03f97c0ee8a364d19862e808cf4c8104f2900abb
KaetiG <kaetigust@gmail.com>
2019-08-02 10:01:20 -0500

    Rate User front end UI done

--- src/components/RateUser/RateUser.js
+++ src/components/RateUser/RateUser.js
@@ -1,48 +1,93 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import { Rating, Grid, Icon, Button } from 'semantic-ui-react'
+import { connect } from 'react-redux';
+import { Rating, Grid, Icon, Button, Modal, Header } from 'semantic-ui-react'
 
 
 class RateUser extends Component {
-  state = {}
-
-  handleRate = (e, { rating, maxRating }) => this.setState({ rating, maxRating }) 
-  
-        
-    render() {
-      return (
-       
-        
-          <Grid>
-            <Grid.Row>
-              <Grid.Column width={16} verticalAlign="center">
-                <h1>Please rate (username)</h1>
-                </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                  <Grid.Column width={16} verticalAlign="center">
-                <Icon name="user circle" size="massive" />
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column width={16} verticalAlign="center">
-                <Rating size="massive" maxRating={5} onRate={this.handleRate} />
-                <pre>{JSON.stringify(this.state, null, 2)}</pre>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column width={16} verticalAlign="center">
-                <Button fluid size="big" color="green">
-                  Submit
-                </Button>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        
-      );
-    }
+  state = {
+    modalOpen: false,
+    rating: 0
   }
-  const mapStateToProps = reduxState => ({
-      reduxState
-  });
-  export default connect(mapStateToProps)(RateUser);
+
+  handleRate = (e, { rating }) => this.setState({ rating })
+
+  handleSubmit = () => {
+    console.log('susie gave you a', this.state.rating)
+  }
+
+  toggleModal = () => {
+    this.setState({
+      ...this.state,
+      modalOpen: !this.state.modalOpen,
+    })
+  }
+
+  handleSkip = () => {
+    console.log('user skipped rating')
+    this.props.history.push('/home')
+  }
+
+  render() {
+    return (
+
+      <>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={16} verticalAlign="center">
+              <h1>Please rate (username)</h1>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={16} verticalAlign="center">
+              <Icon name="user circle" size="massive" />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={16} verticalAlign="center">
+              <Rating size="massive" maxRating={5} onRate={this.handleRate} />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={16} verticalAlign="center">
+              <Button fluid size="big" color="green" onClick={this.handleSubmit}>
+                Submit
+                </Button>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={16} verticalAlign="center">
+              <Button fluid size="big" invertedcolor="grey" onClick={this.toggleModal}>
+                Skip It
+            </Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Modal
+          open={this.state.modalOpen}
+          basic
+          size='small'>
+          <Header content='Are You Sure?' />
+          <Modal.Content>
+            <p>Rating your experience will help other users Budge responsibly.</p>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button color='grey' onClick={this.handleSkip}>
+              No, Thanks
+          </Button>
+            <Button color='green' onClick={this.toggleModal}>
+              Okay, I'll rate.
+          </Button>
+          </Modal.Actions>
+        </Modal>
+      </>
+    )
+  }
+}
+const mapStateToProps = reduxState => ({
+  user: reduxState.user,
+  selectedVenue: reduxState.selectedVenue,
+  selectedSpot: reduxState.selectedSpot,
+  userWaitlist: reduxState.userWaitlist,
+
+});
+export default connect(mapStateToProps)(RateUser);
