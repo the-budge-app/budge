e41393d417fc6a6df1b17ade899f7fd42b7d8c9f
Weizhen Wang <wangwzhen@gmail.com>
2019-08-02 11:05:40 -0500

    move redirect back to venue page from join WL page into saga

--- src/redux/sagas/waitlistJoinLeaveSaga.js
+++ src/redux/sagas/waitlistJoinLeaveSaga.js
@@ -4,6 +4,8 @@ import { takeLatest } from 'redux-saga/effects';
 function* addToWaitlist(action) {    
     try {
         yield axios.post('/api/waitList/join', action.payload);
+        yield action.payload.history.push('/venue/' + this.props.selectedVenue.id);
+
     } catch (error) {
         console.log('Error adding user to waitlist', error)
     }
