cc187b6c1636f7cd385d669a6aa7ca850b1ecf3e
Weizhen Wang <wangwzhen@gmail.com>
2019-08-07 13:32:28 -0500

    display data from redux store on seller confirmation page

--- src/components/Confirmation/SellerConfirm.js
+++ src/components/Confirmation/SellerConfirm.js
@@ -1,46 +1,46 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Grid, Button, Segment } from 'semantic-ui-react'
+import { Grid, Button, Segment, Icon, Rating } from 'semantic-ui-react'
 
 const styles = {
-    icon: {
-      fontSize: '15vw',
-      marginBottom: '10px',
-      marginTop: '10px',
-    },
-    gridRow: {
-      paddingTop: '0',
-      paddingBottom: '0',
-    },
-    partyData: {
-      margin: '0',
-    }
+  icon: {
+    fontSize: '15vw',
+    marginBottom: '10px',
+    marginTop: '10px',
+  },
+  gridRow: {
+    paddingTop: '0',
+    paddingBottom: '0',
+  },
+  partyData: {
+    margin: '0',
   }
+}
 
 class SellerConfirm extends Component {
-    state = {
-        buyer_name: 'Sally1984',
-        offerPrice: 20,
-        userRating: 4.5,
-      }
-    render() {
-        return (
-            <>
-                   <Segment>
+  state = {
+    buyer_name: 'Sally1984',
+    offerPrice: 20,
+    userRating: 4.5,
+  }
+  render() {
+    return (
+      <>
+        <Segment>
           <Grid id='spotData'>
             <Grid.Row>
-              {/* <Grid.Column width={6} textAlign="center">
+              <Grid.Column width={6} textAlign="center">
                 <Icon circular bordered name="user" color="grey" style={styles.icon} />
-                <Rating defaultRating={this.state.userRating} maxRating={5} disabled size='large' />
-                <h5>{this.state.userRating}</h5>
-              </Grid.Column> */}
-              <Grid.Column width={12} style={{ paddingLeft: '0' }}>
+                <Rating rating={this.props.customerRating.rating && this.props.customerRating.rating.substring(0,1)} maxRating={5} disabled size='large' />
+                <h5>{this.props.customerRating.rating && this.props.customerRating.rating.substring(0,3)}</h5>
+              </Grid.Column>
+              <Grid.Column width={10} style={{ paddingLeft: '0' }}>
                 <Grid>
                   <Grid.Row style={styles.gridRow}>
                     <Grid.Column width={16}>
                       <h4>You just got Budged!</h4>
-                      <br/>
-                      <h5>{this.state.buyer_name} paid ${this.state.offerPrice} for your spot</h5>
+                      <br />
+                      <h5>{this.props.buyerInfo.username} paid ${this.props.buyerInfo.offer_price} for your spot</h5>
                     </Grid.Column>
                   </Grid.Row>
                 </Grid>
@@ -48,45 +48,49 @@ class SellerConfirm extends Component {
             </Grid.Row>
           </Grid>
         </Segment>
-        
+
         <Segment>
-        <Grid>
+          <Grid>
             <Grid.Row>
-                <Grid.Column>
+              <Grid.Column>
                 <h3>Your new spot on waitlist:</h3>
-                </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column>
-                        <h4>Reservation Name:</h4>
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column>
-                        <h4>Party Size:</h4>
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column>
-                        <h4>Time to Wait:</h4>
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                <Grid.Column width={12}> 
-                    <Button fluid style={{backgroundColor: 'green', color: 'white', float: 'center'}}>
-                        I'll Take My Money, Thanks!
-                    </Button>
-                </Grid.Column>
+              </Grid.Column>
             </Grid.Row>
-        </Grid>
+            <Grid.Row>
+              <Grid.Column>
+                <h4>Reservation Name: {this.props.buyerInfo.reservation_name}</h4>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+                <h4>Party Size: {this.props.buyerInfo.party_size}</h4>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+                <h4>Time to Wait: {this.props.buyerInfo.latest_wait_time} min</h4>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column width={12}>
+                <Button fluid style={{ backgroundColor: 'green', color: 'white', float: 'center' }}>
+                  I'll Take My Money, Thanks!
+                </Button>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         </Segment>
-            </>
-            
-        )
-    }
+      </>
+
+    )
+  }
 }
 
 const mapStateToProps = reduxState => ({
-    reduxState
-});
+  buyerInfo: reduxState.sellerConfirmation.buyerInfo,
+  selectedVenue: reduxState.selectedVenue,
+  sellerInfo: reduxState.sellerConfirmation.sellerInfo,
+  customerRating: reduxState.customerRating,
+}
+);
 export default connect(mapStateToProps)(SellerConfirm);
