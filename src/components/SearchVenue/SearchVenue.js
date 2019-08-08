9f08609910ab34de055363b65a31738716b0813e
Weizhen Wang <wangwzhen@gmail.com>
2019-08-08 10:03:48 -0500

    build handle input function

--- src/components/SearchVenue/SearchVenue.js
+++ src/components/SearchVenue/SearchVenue.js
@@ -17,11 +17,25 @@ const styles = {
 
 class SearchVenue extends Component {
 
+    state={
+        venueName: '',
+    }
+    //capture the user input and store in local state
+    handleInput = (event) => {
+        this.setState({
+            venueName: event.target.value,
+        })
+    }
+    //function to run the search
+    handleSearch = () => {
+        console.log(this.state)
+    }
+
     render() {
         return(
             <div style={styles.search}>
-                <Input placeholder="Search a venue"/>
-                <Button primary style={styles.button}>Go</Button>
+                <Input placeholder="Search a venue" onChange={this.handleInput} value={this.state.venueName}/>
+                <Button primary style={styles.button} onClick={this.handleSearch}>Go</Button>
                 <Button secondary style={styles.button}>Reset</Button>
             </div>
         )
