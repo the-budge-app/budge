17bca7d1a21d3770c6479a8884f8fe9662236415
Kyle Henderson <kthenderson83@gmail.com>
2019-08-10 18:13:21 -0500

    added all functionality to the add payment page along with autofilling the inputs

--- src/components/PaymentPage/AddPaymentTab.js
+++ src/components/PaymentPage/AddPaymentTab.js
@@ -1,64 +1,124 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Button, Input } from 'semantic-ui-react'
+import { Grid, Button, Input } from 'semantic-ui-react'
 
 const styles = {
     mainDiv: {
-
-        textAlign: 'center',
         maxWidth: '900px',
         marginLeft: 'auto',
         marginRight: 'auto',
-    },
-    paymentDiv: {
-        textAlign: 'left',
-        marginLeft: '12%',
-    },
-    cvv: {
-        float: 'right',
-        maxWidth: '15%',
-        marginRight: '2%',
     }
 }
 
 class AddPaymentTab extends Component {
 
+    state = {
+        name: '',
+        number: '',
+        expiration: '',
+        cvv: '',
+        zip: '',
+    }
+
+    autofillPayment = () => {
+        this.setState({
+            name: 'Kaeti G',
+            number: '1234567812345678',
+            expiration: '02/25',
+            cvv: '123',
+            zip: '55415',
+        })
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            ...this.state, [event.target.id]: event.target.value
+        })
+    }
+
+    handleSubmit = () => {
+        // add modal here for success
+        console.log(this.state)
+        this.setState({
+            name: '',
+            number: '',
+            expiration: '',
+            cvv: '',
+            zip: '',
+        })
+
+    }
 
     render() {
         return (
-            <div style={styles.mainDiv} >
-                <br />
-                <br />
-                <label>Name on Credit Card</label>
-                <br />
-                <Input type='text' placeholder='John M Smith'></Input>
-                <br />
-                <br />
-                <label>Card Number</label>
-                <br />
-                <Input type='text' placeholder='xxxx-xxxx-xxxx-0000'></Input>
-                <br />
-                <br />
-                <label>Expiration Date</label>
-                <br />
-                <Input type='text' placeholder='01/01/2023'></Input>
-                <br />
-                <br />
-                <label>CVV CODE</label>
-                <br />
-                <Input type='text' placeholder='123' ></Input>
-                <br />
-                <br />
-                <label>Billing Zip Code</label>
-                <br />
-                <Input type='text' placeholder='55415'></Input>
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <Button fluid style={{ backgroundColor: 'green', color: 'white' }}>Add Payment Method</Button>
+            <div style={styles.mainDiv} id="addPayment">
+                <Grid centered>
+                    <Grid.Row>
+                        <Grid.Column width={16}>
+                            <Input
+                                value={this.state.name}
+                                onChange={this.handleChange}
+                                id="name"
+                                fluid
+                                label={{ basic: true, content: 'Name on Card', onClick: () => this.autofillPayment() }}
+                                placeholder='Cardholder name'
+                            />
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={16}>
+                            <Input
+                                value={this.state.number}
+                                onChange={this.handleChange}
+                                id="number"
+                                fluid
+                                label={{ basic: true, content: 'Card Number' }}
+                                placeholder='Card Number'
+                            />
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={16}>
+                            <Input
+                                value={this.state.expiration}
+                                onChange={this.handleChange}
+                                id="expiration"
+                                fluid
+                                label={{ basic: true, content: 'Exp Date' }}
+                                placeholder='Exp Date(mm/yy)'
+                            />
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={16}>
+                            <Input
+                                value={this.state.cvv}
+                                onChange={this.handleChange}
+                                id="cvv"
+                                fluid
+                                label={{ basic: true, content: 'CVV' }}
+                                placeholder='CVV'
+                            />
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={16}>
+                            <Input
+                                value={this.state.zip}
+                                onChange={this.handleChange}
+                                id="zip"
+                                fluid
+                                label={{ basic: true, content: 'Zip Code' }}
+                                placeholder='Zip Code'
+                            />
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+                <Grid centered>
+                    <Grid.Column width={12}>
+                        <Button onClick={this.handleSubmit} fluid color="green">Add Payment Method</Button>
+                    </Grid.Column>
+                </Grid>
             </div>
         )
     }
